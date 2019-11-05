5364ba8eb98cc4b39d599cdff4603b4e55e6639f
siamkarim <49981700+siamkarim@users.noreply.github.com>
2019-11-05 11:22:35 +0600

    chapter 4 case studies

---
+++ inventory_report.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#define ITEMS 4
+main(){
+
+    int i, quantity[5];
+    float rate[5],value,total_value;
+    char code[5][5];
+    i=1;
+
+    while (i<=ITEMS) {
+
+        printf("Enter code, Quantity, and Rate:");
+        scanf("%s %d %f",&code[i],&quantity[i],&rate[i]);
+        i++;
+    }
+    printf("\n\n");
+    printf("          INVENTORY REPORT        \n");
+    printf("---------------------------------\n");
+    printf(" Code   Quantity    Rate     Value    \n");
+    printf("---------------------------------\n");
+
+    total_value = 0;
+    i = 1;
+
+    while ( i <= ITEMS) {
+
+        value = quantity[i]*rate[i];
+        printf("%5s %10d %10.2f %e\n",code[i],quantity[i],rate[i],value);
+        total_value += value;
+        i++;
+
+    }
+    printf("----------------------------------\n");
+    printf("               Total Value = %e\n", total_value);
+    printf("----------------------------------\n");
+
+}
